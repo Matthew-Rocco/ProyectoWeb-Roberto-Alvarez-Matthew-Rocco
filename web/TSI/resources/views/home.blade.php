ada14d21e0892371f7432337bec6f4865fb47ff8
Matthew <matt.rt30@gmail.com>
2021-07-15 03:27:29 -0400

    Menu inicial

---
+++ web/TSI/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('contenido')
+    <div class="row pt-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header row bg-info bg-gradient">
+                    <div class="col-10 pt-2">
+                      <div>
+                        <h6>Almacenamiento</h6>
+                      </div>
+                    </div>
+                    <div class="col-2 text-end">
+                      <div>
+                        <a class="nav-link text-dark" href="{{ route('almacenamiento') }}">Ver más</a>
+                      </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{asset('img/ssd.jpg')}}" class="card-img-top img-fluid" alt="">
+                        <div class="card-body">
+                            <p class="card-text">WD® Unidad SSD 120GB Sata3 2.5" Green</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer bg-info">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ web/TSI/resources/views/mi_lista.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('contenido')
+  <h1>Estas son las listas</h1>
+@endsection
